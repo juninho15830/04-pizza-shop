f60fbc52f2b394b8eb206f47c545d9b488dabe7c
Jair de Jesus Redigolo Junior <32220266+juninho15830@users.noreply.github.com>
2024-11-18 18:57:14 -0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { Button } from './components/ui/button'
+
+export function App() {
+
+  return (
+    <div className='flex items-center justify-center h-screen'>
+      Hello World
+      <Button>Start</Button>
+    </div>
+  )
+}
