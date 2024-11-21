4c7fb79445c99bd77cba6a9d41a7c127d840653a
Jair de Jesus Redigolo Junior <32220266+juninho15830@users.noreply.github.com>
2024-11-21 16:42:06 -0300

    add toast and sign-up

--- src/pages/auth/sign-in.tsx
+++ src/pages/auth/sign-in.tsx
@@ -5,6 +5,8 @@ import * as z from 'zod'
 
 import { Button } from '@/components/ui/button'
 import { Input } from '@/components/ui/input'
+import { toast } from 'sonner'
+import { Link } from 'react-router-dom'
 
 const signInForm = z.object({
   email: z.string().email(),
@@ -20,14 +22,31 @@ export function SignIn() {
   } = useForm<SignInForm>()
 
   async function handleSignIn(data: SignInForm) {
-    console.log(data)
-    await new Promise((resolve) => setTimeout(resolve, 2000))
+    try {
+      console.log(data)
+
+      await new Promise((resolve) => setTimeout(resolve, 2000))
+
+      toast.success('Enviamos um link de autenticação para o seu e-mail.', {
+        action: {
+          label: 'Reenviar',
+          onClick: () => handleSignIn(data),
+        }
+      })
+    } catch {
+      toast.error('Credenciais invalidas.')
+    }
   }
 
   return (
     <>
       <Helmet title="Login" />
       <div className="p-8">
+        <Button variant="outline" asChild className='absolute right-8 top-8'>
+          <Link to="/sign-up">
+            Novo estabelecimento
+          </Link>
+        </Button>
         <div className="flex w-[350px] flex-col justify-center gap-6">
           <div className="flex flex-col gap-2 text-center">
             <h1 className="text-2xl font-semibold tracking-tight">
