bc2f0f553b791dafd051853e61efae116a38668e
Jair de Jesus Redigolo Junior <32220266+juninho15830@users.noreply.github.com>
2024-11-26 15:28:29 -0300

    create env

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,8 @@ import { Toaster } from 'sonner'
 
 import { router } from './routes'
 import { ThemeProvider } from './components/theme/theme-provider'
+import { QueryClientProvider } from '@tanstack/react-query'
+import { queryClient } from './lib/react-query'
 
 export function App() {
   return (
@@ -11,7 +13,9 @@ export function App() {
       <ThemeProvider defaultTheme="system" storageKey="pizzashop-theme" >
         <Helmet titleTemplate="%s | pizza.shop" />
         <Toaster richColors/>
-        <RouterProvider router={router} />
+        <QueryClientProvider client={queryClient}>
+          <RouterProvider router={router} />
+        </QueryClientProvider>
       </ThemeProvider>
     </HelmetProvider>
   )
