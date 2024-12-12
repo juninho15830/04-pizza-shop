c5414580a8405aa0c1e31bc9e650010f9a9a375a
Jair de Jesus Redigolo Junior <32220266+juninho15830@users.noreply.github.com>
2024-12-11 21:04:23 -0300

    tests

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import path from "node:path"
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import type { UserConfig } from "vite"
-import type { InlineConfig } from "vite"
+import type { InlineConfig } from "vitest/node"
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -14,6 +14,8 @@ export default defineConfig({
   },
   test: {
     globals: true,
+    setupFiles: ['./test/setup.ts'],
+    enviroment: 'happy-dom',
   }
 } as UserConfig & {
   test: InlineConfig
