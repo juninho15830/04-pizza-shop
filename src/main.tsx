1f1c1515bf805885a9527e74bbc7d23860189ade
Jair de Jesus Redigolo Junior <32220266+juninho15830@users.noreply.github.com>
2024-12-13 19:40:10 -0300

    install msw (mock)

--- src/main.tsx
+++ src/main.tsx
@@ -4,9 +4,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 
 import { App } from './app.tsx'
+import { enableMSW } from './api/mocks/index.ts'
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+enableMSW().then(() => {
+  createRoot(document.getElementById('root')!).render(
+    <StrictMode>
+      <App />
+    </StrictMode>,
+  )
+})
